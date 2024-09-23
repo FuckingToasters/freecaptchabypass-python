0d056fe3b47af2a053dc1bd00f56dc13cc2def90
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2024-09-23 19:03:52 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="freecaptchabypass",
+    version="0.1",
+    description="A Wrapper for fcb captchs solving service",
+    packages=["freecaptchabypass"],
+    install_requires=["requests"],
+)
